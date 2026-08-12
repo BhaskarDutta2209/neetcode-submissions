f4301336ba8ddd1218d733efc501b59b88de8832
Bhaskar <43211224+BhaskarDutta2209@users.noreply.github.com>
2026-08-12 13:16:26 +0530

    Add: two-integer-sum-ii - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-6.rs
@@ -0,0 +1,23 @@
+impl Solution {
+    pub fn two_sum(numbers: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut low = 0;
+        let mut high = numbers.len() as i32 - 1;
+
+        while low <= high {
+            // println!("Low: {} High: {}", low, high);
+            let sum = numbers.get(low as usize).unwrap() + numbers.get(high as usize).unwrap();
+
+            if sum < target {
+                low += 1;
+            } else if sum > target {
+                high -= 1;
+            } else {
+                return vec![low+1, high+1];
+            }
+        }
+
+        // println!("WHAT???");
+
+        return vec![1,2]
+    }
+}
