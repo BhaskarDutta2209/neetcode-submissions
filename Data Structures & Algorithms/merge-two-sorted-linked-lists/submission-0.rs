1de28d77cd34e95ec8df38f08b0f6078589c4009
Bhaskar <43211224+BhaskarDutta2209@users.noreply.github.com>
2026-09-04 15:08:25 +0530

    Add: merge-two-sorted-linked-lists - submission-0

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-0.rs
@@ -0,0 +1,20 @@
+impl Solution {
+    pub fn merge_two_lists(
+        list1: Option<Box<ListNode>>,
+        list2: Option<Box<ListNode>>,
+    ) -> Option<Box<ListNode>> {
+        match (list1, list2) {
+            (None, r) => r,
+            (l, None) => l,
+            (Some(mut l1), Some(mut l2)) => {
+                if l1.val <= l2.val {
+                    l1.next = Self::merge_two_lists(l1.next, Some(l2));
+                    Some(l1)
+                } else {
+                    l2.next = Self::merge_two_lists(Some(l1), l2.next);
+                    Some(l2)
+                }
+            }
+        }
+    }
+}
